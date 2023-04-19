9e51ded4b85a2628034bbaeb0f44732a492be564
Aurelia Segarra <av.segarra@gmail.com>
2023-04-19 21:19:24 +0200

    Music schema created

---
+++ db/models/musics.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from db.base_class import Base
+
+
+class Music(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    artist = Column(String, nullable=False)
+    album = Column(String, nullable=False)
+    release_date = Column(Date, nullable=False)
+    is_favorite = Column(Boolean, default=False)
+    is_active = Column(Boolean, default=True)
+    owner_id = Column(Integer, ForeignKey("user.id"))
+    owner = relationship("User", back_populates="musics")
