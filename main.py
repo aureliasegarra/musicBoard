6609c662225c75c6f0806b8981be5aec873f7cab
Aurelia Segarra <av.segarra@gmail.com>
2023-04-19 23:45:36 +0200

    Create User endpoint OK

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from fastapi import FastAPI
-from core.config import Settings
+from core.config import settings
 from db.session import engine
 from db.base import Base
+from apis.base import api_router
 
 
 def create_tables():
     Base.metadata.create_all(bind=engine)
 
 
+def include_router(app):
+    app.include_router(api_router)
+
+
 def start_application():
-    app = FastAPI(title=Settings.PROJECT_TITLE, description=Settings.PROJECT_DESCRIPTION,
-                  version=Settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_TITLE, description=settings.PROJECT_DESCRIPTION,
+                  version=settings.PROJECT_VERSION)
     create_tables()
+    include_router(app)
     return app
 
 
