dce738dafd54b3930b847073fbeab0d63959f644
Aurelia Segarra <av.segarra@gmail.com>
2023-04-19 20:41:12 +0200

    Connection to database OK

---
+++ core/config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
+class Settings:
+    PROJECT_TITLE: str = "Music Board"
+    PROJECT_VERSION: str = "1.0.0"
+    PROJECT_DESCRIPTION: str = "Small project to improve fastapi"
+
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from core.config import Settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title="Music Board", description="Small project to improve fastapi ", version="1.0.0")
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=Settings.PROJECT_TITLE, description=Settings.PROJECT_DESCRIPTION,
+                  version=Settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
-
+    return {"message": "Bienvenue sur Music Board"}
