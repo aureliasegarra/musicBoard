5d88ceef3b301a78eb1f9c85a77b632cf94a39e3
Aurelia Segarra <av.segarra@gmail.com>
2023-04-20 08:17:10 +0200

    Hide unecessaries details in create user endpoint

--- schemas/users.py
+++ schemas/users.py
@@ -6,3 +6,15 @@ class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+
+
+
+#The ShowUser class is used to return only the necessary user data to the client.
+class ShowUser(BaseModel):
+    username: str
+    email: EmailStr
+    is_active: bool
+
+    class Config():
+        orm_mode = True
