25cd313d853293d3c8166fcc9797431369209b1d
Aurelia Segarra <av.segarra@gmail.com>
2023-04-20 17:39:59 +0200

    API endpoint to update music by id

--- apis/version1/route_musics.py
+++ apis/version1/route_musics.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from db.models.musics import Music
 from schemas.musics import MusicCreate, ShowMusic
-from db.repository.musics import create_new_music, retreive_music, list_musics
+from db.repository.musics import create_new_music, retreive_music, list_musics, update_music_by_id
 from typing import List
 
 router = APIRouter()
@@ -30,5 +30,15 @@ def retreive_music_by_id(id: int, db: Session = Depends(get_db)):
 def retreive_all_musics(db: Session = Depends(get_db)):
     musics = list_musics(db=db)
     return musics
+
+
+@router.put("/update/{id}")
+def update_music(id:int, music:MusicCreate, db:Session=Depends(get_db)):
+    owner_id = 1
+    message = update_music_by_id(id=id, music=music, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The music with the id {id} is not available")
+    return {"detail": "Music updated successfully"}
 
 
--- db/repository/musics.py
+++ db/repository/musics.py
@@ -13,10 +13,20 @@ def create_new_music(music: MusicCreate, db: Session, owner_id: int):
 
 
 def retreive_music(id: int, db: Session):
-    music = db.query(Music).filter(Music.is_active==True).all()
+    music = db.query(Music).filter(Music.is_active == True).all()
     return music
 
 
 def list_musics(db: Session):
     musics = db.query(Music).all()
     return musics
+
+
+def update_music_by_id(id: int, music: MusicCreate, db: Session, owner_id: int):
+    existing_music = db.query(Music).filter(Music.id == id)
+    if not existing_music.first():
+        return 0
+    music.__dict__.update(owner_id=owner_id)
+    existing_music.update(music.__dict__)
+    db.commit()
+    return 1
