6db15752758955a92200862a1926784e9a6faab7
Aurelia Segarra <av.segarra@gmail.com>
2023-04-21 10:18:43 +0200

    API endpoint to delete music OK

--- apis/version1/route_musics.py
+++ apis/version1/route_musics.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db
 from db.models.musics import Music
 from schemas.musics import MusicCreate, ShowMusic
-from db.repository.musics import create_new_music, retreive_music, list_musics, update_music_by_id
+from db.repository.musics import create_new_music, retreive_music, list_musics, update_music_by_id, delete_music_by_id
 from typing import List
 
 router = APIRouter()
@@ -22,7 +22,7 @@ def retreive_music_by_id(id: int, db: Session = Depends(get_db)):
     music = retreive_music(id=id, db=db)
     if not music:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"The music with the id {id} is not available")
+                            detail=f"The music with the id {id} is not available")
     return music
 
 
@@ -33,12 +33,20 @@ def retreive_all_musics(db: Session = Depends(get_db)):
 
 
 @router.put("/update/{id}")
-def update_music(id:int, music:MusicCreate, db:Session=Depends(get_db)):
+def update_music(id: int, music: MusicCreate, db: Session = Depends(get_db)):
     owner_id = 1
     message = update_music_by_id(id=id, music=music, db=db, owner_id=owner_id)
     if not message:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"The music with the id {id} is not available")
+                            detail=f"The music with the id {id} is not available")
     return {"detail": "Music updated successfully"}
 
 
+@router.delete("/delete/{id}")
+def delete_music(id: int, db: Session = Depends(get_db)):
+    owner_id = 1
+    message = delete_music_by_id(id=id, db=db, owner_id=owner_id)
+    if not message:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"The music with the id {id} is not available")
+    return {"detail": "Music deleted successfully"}
--- db/repository/musics.py
+++ db/repository/musics.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import Session
-
 from schemas.musics import MusicCreate, ShowMusic
 from db.models.musics import Music
 
 
+""" FUNCTION TO POST NEW MUSIC """
 def create_new_music(music: MusicCreate, db: Session, owner_id: int):
     music = Music(**music.dict(), owner_id=owner_id)
     db.add(music)
@@ -12,21 +12,34 @@ def create_new_music(music: MusicCreate, db: Session, owner_id: int):
     return music
 
 
+""" FUNCTION TO GET MUSIC BY ID """
 def retreive_music(id: int, db: Session):
     music = db.query(Music).filter(Music.is_active == True).all()
     return music
 
 
+""" FUNCTION TO GET ALL MUSIC"""
 def list_musics(db: Session):
     musics = db.query(Music).all()
     return musics
 
 
+""" FUNCTION TO UPDATE MUSIC BY ID """
 def update_music_by_id(id: int, music: MusicCreate, db: Session, owner_id: int):
     existing_music = db.query(Music).filter(Music.id == id)
     if not existing_music.first():
         return 0
     music.__dict__.update(owner_id=owner_id)
     existing_music.update(music.__dict__)
+    db.commit()
+    return 1
+
+
+""" FUNCTION TO DELETE MUSIC BY ID """
+def delete_music_by_id(id: int, db: Session, owner_id: int):
+    existing_music = db.query(Music).filter(Music.id == id)
+    if not existing_music.first():
+        return 0
+    existing_music.delete(synchronize_session=False)
     db.commit()
     return 1
