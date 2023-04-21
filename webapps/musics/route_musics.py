489b58a4a3e377e3078e1c48698f3089a96601b1
Aurelia Segarra <av.segarra@gmail.com>
2023-04-21 17:57:47 +0200

    Display list of musics OK

--- webapps/musics/route_musics.py
+++ webapps/musics/route_musics.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
-from fastapi import Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from db.repository.musics import list_musics
+from sqlalchemy.orm import Session
+from db.session import get_db
 
 
 templates = Jinja2Templates(directory="templates")
@@ -9,6 +11,6 @@ router = APIRouter(include_in_schema=False)
 
 
 @router.get("/", response_class=HTMLResponse)
-def home(request: Request):
-    print(dir(request))
-    return templates.TemplateResponse("musics/homepage.html", {"request": request})
+def home(request: Request, db: Session = Depends(get_db)):
+    musics = list_musics(db=db)
+    return templates.TemplateResponse("musics/homepage.html", {"request": request, "musics": musics})
