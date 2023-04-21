12c89114812936abe3f21dc6c1efbb1bc47d40fe
Aurelia Segarra <av.segarra@gmail.com>
2023-04-21 21:59:36 +0200

    Registration OK

--- webapps/base.py
+++ webapps/base.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-
 from webapps.musics import route_musics
+from webapps.users import route_users
 
 api_router = APIRouter()
 
 api_router.include_router(route_musics.router, prefix="", tags=["homepage"])
+api_router.include_router(route_users.router, prefix="", tags=["users"])
